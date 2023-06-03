2fbb54e60fc2932667a0924d2ce238edfcfc9019
Priyamvada-k <133840412+Priyamvada-k@users.noreply.github.com>
2023-06-03 23:29:21 +0530

    Update code.py

--- code.py
+++ code.py
@@ -76,4 +76,78 @@ except Exception as e:
 
 lnwriter = csv.writer(f)
 
+try:
+    lnwriter.writerow(["Names", "Time of Entrance"])
+
+    while True:
+        ret, frame = video_capture.read()
+        if not ret:
+            print("Failed to capture video frame")
+            break
+
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        if s:
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame,
+                                                             face_locations)
+            face_names = []
+
+            for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces(known_face_encodings,
+                                                         face_encoding,
+                                                         tolerance=threshold)
+                name = ""
+                abc = known_face_encodings
+                face_distances = face_recognition.face_distance(abc,
+                                                                face_encoding)
+
+                if min(face_distances) < 0.5:
+                    best_match_index = np.argmin(face_distances)
+
+                    if matches[best_match_index]:
+                        name = known_face_names[best_match_index]
+                else:
+                    name = "Unknown"
+
+                face_names.append(name)
+
+                if name in face_names:
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    bottomLeftCornerOfText = (10, 100)
+                    fontScale = 1.5
+                    fontColor = (255, 0, 0)
+                    thickness = 3
+                    lineType = 2
+                    cv2.putText(frame, name,
+                                bottomLeftCornerOfText,
+                                font,
+                                fontScale,
+                                fontColor,
+                                thickness,
+                                lineType)
+
+                    if name in students:
+                        students.remove(name)
+                        current_time = now.strftime("%H-%M-%S")
+                        lnwriter.writerow([name, current_time])
+
+        cv2.imshow("Attendance System", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+except Exception as e:
+    print(f"An error occurred during video processing: {e}")
+
+finally:
+    video_capture.release()
+    cv2.destroyAllWindows()
+    f.close()
+
+try:
+    analyze_attendance("attendance_files/"+current_date + '.csv')
+except Exception as e:
+    print(f"Failed to analyze attendance: {e}")
 
